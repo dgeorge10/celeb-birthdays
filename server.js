8d17c520a990a6333b04a49ca93fd43fe090f8e4
Dennis George <djg365@drexel.edu>
2019-06-01 22:32:32 -0400

    basic endpoint to get celebreties by a certain date

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const parser  = require('node-html-parser')
+const request = require('request');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
+
+app.get('/getBirthday' , (req, res) => {
+	let { birthday } = req.query;
+	people = []
+
+	request("https://celebritybucks.com/tools/birthdays/" + birthday, (err, response, body) => {
+		if (err) { res.send(err); return;}
+		const root = parser.parse(body)
+		let regex = /alt="([\w\s]*)"/g
+		matches = body.match(regex)
+		for(let i = 1; i < matches.length; i++){
+			let raw = matches[i]
+			let split = raw.split("=")[1]
+			if(split.includes("Tweet")) { 
+				console.log("twitter button found")		
+				continue; 
+			}
+			let name = split.replace(/['"]+/g, '')
+			people.push(name)
+		}
+		console.log(people)
+		res.send(people)
+
+	})
+});
--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
+class App extends React.Component {
+	constructor(props){
+		super(props);
+		this.getDate = this.getDate.bind(this)
+	}
+	getDate(){
+			let birthdate = document.getElementById("date").value 
+			fetch("/getBirthday?birthday=" + birthdate)
+			.then(results => {
+				results.text().then(data => console.log(data))
+			})
+		}
+    render(){
+        return <div className="App">
             <div className="Container">
               <div className="Header">Celebrity Birthdays</div>
-              <input className="InputBox" type="text" placeholder="Enter Your Birthday Here (MM/DD/YY)"></input>
+              <input id="date" className="InputBox" type="text" placeholder="Enter Your Birthday Here (YYYY-DD-MM)"></input>
               <div>
-                  <button className="SearchButton">Search</button>
+                  <button onClick={this.getDate} className="SearchButton">Search</button>
               </div>
             </div>
         </div>
-      );
+      }
     }
 
 export default App;
