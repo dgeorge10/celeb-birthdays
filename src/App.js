d7ce12a1c907c6fe99622a4e0adb8a52ad13b994
Hoang Nguyen <hn374@drexel.edu>
2019-06-01 22:57:53 -0400

    Fixed placeholder text and formatting App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
 	constructor(props){
 		super(props);
-		this.getDate = this.getDate.bind(this)
+		this.getDate = this.getDate.bind(this);
 	}
+
 	getDate(){
-			let birthdate = document.getElementById("date").value 
+			let birthdate = document.getElementById("date").value;
+
 			fetch("/getBirthday?birthday=" + birthdate)
 			.then(results => {
 				results.text().then(data => console.log(data))
 			})
 		}
+
     render(){
         return <div className="App">
             <div className="Container">
               <div className="Header">Celebrity Birthdays</div>
-              <input id="date" className="InputBox" type="text" placeholder="Enter Your Birthday Here (YYYY-DD-MM)"></input>
+              <input id="date" className="InputBox" type="text" placeholder="Enter Your Birthday Here (YYYY-MM-DD)"></input>
               <div>
                   <button onClick={this.getDate} className="SearchButton">Search</button>
               </div>
