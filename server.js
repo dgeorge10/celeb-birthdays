a1dc1c6a2ca572f6457e4acfa213f7350c4f08e6
Dennis George <djg365@drexel.edu>
2019-06-11 19:52:01 -0400

    changing static path

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const parser  = require('node-html-parser')
 const request = require('request');
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require("path")
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+
 
 app.get('/getBirthday' , (req, res) => {
 	let { birthday } = req.query;
